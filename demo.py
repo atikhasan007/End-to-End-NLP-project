6801042867bed587e3b6b24144d09d0b5fe1e99c
Md Atik Hasan <atikhasan007@gmail.com>
2026-04-26 10:41:47 +0600

    exception setup

--- demo.py
+++ demo.py
@@ -1,4 +1,12 @@
 
 from hate.logger import logging
+from hate.exception import CustomException
+import sys
 
-logging.info("welcome to our project")
+
+#logging.info("welcome to our project")
+
+try:
+    a = 5/2
+except Exception as e:
+    raise CustomException(e,sys)
